baf2ff665b4f5df99d46acd4779a718db75da523
Basil Abu-Ragheef <basilaburagheef@gmail.com>
2023-03-15 17:10:40 +0100

    debug toolbar fix

--- testaiproject/settings.py
+++ testaiproject/settings.py
@@ -151,3 +151,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
