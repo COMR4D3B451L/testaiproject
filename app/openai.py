1ea55e2d185c88d4aa89c666b98c2949f0ae9d82
api <basilaburagheef@gmail.com>
2023-03-19 00:17:50 +0100

    chat continuation

--- app/openai.py
+++ app/openai.py
@@ -2,13 +2,23 @@ from django.conf import settings
 import openai
 
 
-def ai_call(input_text, word_count, initial_message):
+def messages(request, input_text, word_count, initial_message):
+    messages=[
+                {"role": "system", "content": f"{initial_message}, give me an answer in max {word_count} words."},
+                {"role": "user", "content": input_text},
+                ]
+    if not request.session['message_history']:
+        request.session['message_history'] = messages
+        return messages
+    else:
+        messages = request.session['message_history']
+        messages.append({"role": "user", "content": input_text})
+        return messages
+
+def ai_call(messages: list):
     openai.api_key = settings.OPEN_AI_API_KEY
     ans = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                    {"role": "system", "content": f"{initial_message}, give me an answer in max {word_count} words."},
-                    {"role": "user", "content": input_text},
-                ]
+            messages=messages
             )
     return ans
--- app/views.py
+++ app/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.views.generic import TemplateView
 from .forms import ChatBotForm
-from .openai import ai_call
+from .openai import ai_call, messages
+import json
 
 class index(TemplateView):
     template_name = 'pages/main/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        request.session['message_history'] = None
+        return super().get(self, request, *args, **kwargs)
 
 def post(request):
     if request.method == 'POST':
@@ -17,8 +22,11 @@ def post(request):
             input_text = data['input_text']
             word_count = data['word_count']
             initial_message = data['initial_message']
-            resp = ai_call(input_text, word_count, initial_message)
+            msg = messages(request, input_text, word_count, initial_message)
+            resp = ai_call(msg)
             content = resp['choices'][0]['message']['content']
+            resp_dict = json.loads(str(resp['choices'][0]['message']))
+            request.session['message_history'].append(resp_dict)
             chat_bot_form.answer_text = content
             chat_bot_form.save()
             
